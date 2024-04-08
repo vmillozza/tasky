9a3e633e5639e3f34c68efa1989dc50ab4d2f6c0
vmillozza <142731369+vmillozza@users.noreply.github.com>
2024-04-08 09:53:50 +0200

    Auth

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,17 +3,25 @@ import Home from './pages/Home';
 import SignIn from './pages/Signln';
 import SignUp from './pages/SignUp';
 import { ChakraProvider } from '@chakra-ui/react'
-import { Toaster} from 'react-hot-toast' ;
+//import { Toaster } from 'react-hot-toast';
+import { UserProvider } from './context/UserContext';
+import Profile from './pages/Profile' ;
+import PrivateRoute from './components/PrivateRoute';
 export default function App() {
   return (
-    <ChakraProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/signin' element={<SignIn />} />
-          <Route path='/signup' element={<SignUp />} />
-        </Routes>
-      </BrowserRouter>
-    </ChakraProvider>
+    <UserProvider>
+      <ChakraProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/signin' element={<SignIn />} />
+            <Route path='/signup' element={<SignUp />} />
+            <Route element={<PrivateRoute />}>
+              <Route path='/profile' element={<Profile />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </ChakraProvider>
+    </UserProvider>
   );
 }
